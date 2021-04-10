85a8379a35e02607771df36e840bf64f28f8195f
Kamil Khismatullin <kami16ru@gmail.com>
2021-04-10 20:02:39 +0300

    404 error page

--- nuxt.config.js
+++ nuxt.config.js
@@ -46,5 +46,14 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+  },
+  router: {
+    extendRoutes(routes, resolve) {
+      routes.push({
+        name: '404',
+        path: '*',
+        component: resolve(__dirname, 'pages/404.vue')
+      })
+    }
   }
 }
