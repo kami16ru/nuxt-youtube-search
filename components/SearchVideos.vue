3959c3e1b82d3b04a839adbdeda7a8aecee3d3b3
Kamil Khismatullin <kami16ru@gmail.com>
2021-04-12 09:48:52 +0300

    api requests refactored

--- components/SearchVideos.vue
+++ components/SearchVideos.vue
@@ -17,7 +17,8 @@
         alt="Spinning youtube logo"
         width="24px"
         height="24px"
-        class="mx-auto"/>
+        class="mx-auto"
+      >
       <svg v-if="!loading" class="mx-auto fill-current text-gray-400" style="width:24px;height:24px" viewBox="0 0 24 24">
         <path fill="currentColor" d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z" />
       </svg>
@@ -35,8 +36,7 @@ export default {
     searchParams: {
       q: '',
       maxResults: 10,
-      order: 'date',
-      key: process.env.NUXT_ENV_GOOGLE_API_KEY
+      order: 'date'
     },
     responseErrors: [],
     searchRequests: []
